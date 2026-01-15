c17c0a2ba7a60196fb227fb074c6186f2c33e794
Enzo Bom <enzobom@gmail.com>
2026-01-15 16:07:30 -0300

    First commit

---
+++ src/test/java/com/javanauta/ts/bff/BffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.ts.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
